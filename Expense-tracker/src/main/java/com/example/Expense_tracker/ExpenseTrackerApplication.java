0002615b01b2cf7fd4a1d452a42093ffb229302f
GfromMacBook <gwuhou@ucsd.edu>
2024-06-27 07:50:18 -0700

    finished expense tracker:

--- Expense-tracker/src/main/java/com/example/Expense_tracker/ExpenseTrackerApplication.java
+++ Expense-tracker/src/main/java/com/example/Expense_tracker/ExpenseTrackerApplication.java
@@ -1,7 +1,35 @@
 package com.example.Expense_tracker;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Expense Tracker REST API Documentations",
+				description = "Expense Tracker REST API documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Gil",
+						email = "fake@hotmail.com",
+						url = "https://www.javaguides.net"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.javaguides.net/license"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Expense Tracker REST API Documentation for developers",
+				url = "https://www.javaguides.net/external-doc.html"
+		)
+)
+
 
 @SpringBootApplication
 public class ExpenseTrackerApplication {
