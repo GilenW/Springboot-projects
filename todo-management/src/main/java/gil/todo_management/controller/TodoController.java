3437ada3bf44030ef2aeb8ce59881217b6a0241e
GfromMacBook <gwuhou@ucsd.edu>
2024-06-27 20:17:54 -0700

    work with database authenticationOD

--- todo-management/src/main/java/gil/todo_management/controller/TodoController.java
+++ todo-management/src/main/java/gil/todo_management/controller/TodoController.java
@@ -7,6 +7,7 @@ import gil.todo_management.service.TodoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,12 +20,14 @@ public class TodoController {
 
     private TodoService todoService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
         TodoDto savedTodo = todoService.addTodo(todoDto);
         return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("{id}")
     public ResponseEntity<TodoDto> getTodo(@PathVariable("id") Long todoId){
         TodoDto todoDto = todoService.getTodo(todoId);
@@ -32,13 +35,14 @@ public class TodoController {
 
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping
     public ResponseEntity<List<TodoDto>> getAllTodos(){
         List<TodoDto> todoDtos = todoService.getAllTodos();
         return ResponseEntity.ok(todoDtos);
     }
 
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public ResponseEntity<TodoDto> updateTodo(@RequestBody
                                               TodoDto todoDto, @PathVariable("id") Long todoId){
@@ -49,12 +53,14 @@ public class TodoController {
 
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteTodo(@PathVariable("id") Long todoId){
         todoService.deleteTodo(todoId);
         return ResponseEntity.ok("Todo deleted successfully!");
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("{id}/complete")
     public ResponseEntity<TodoDto> completeTodo(@PathVariable("id")
                                                 Long todoId){
@@ -62,6 +68,7 @@ public class TodoController {
         return ResponseEntity.ok(todoDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("{id}/in-complete")
     public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable("id")
                                                 Long todoId){
---
+++ todo-management/src/main/java/gil/todo_management/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package gil.todo_management.repository;
+
+import gil.todo_management.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+
+}
