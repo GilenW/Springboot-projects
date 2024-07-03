fe7ccded08f3e289e1511d330309079788bff0aa
gilenAquillius <gwuhou@ucsd.edu>
2024-07-03 10:30:44 -0700

    revert

--- Student-Management-Project/src/main/java/com/example/studentManagement/controller/StudentController.java
+++ Student-Management-Project/src/main/java/com/example/studentManagement/controller/StudentController.java
@@ -3,8 +3,10 @@ package com.example.studentManagement.controller;
 
 import com.example.studentManagement.dto.StudentDto;
 import com.example.studentManagement.service.StudentService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,11 +39,18 @@ public class StudentController {
 
     //handler method to handle save student form submit request
     @PostMapping("/students")
-    public String saveStudent(@ModelAttribute("student") StudentDto studentDto){
+    public String saveStudent(@Valid @ModelAttribute("student") StudentDto studentDto,
+                              BindingResult result,
+                              Model model){
+
+        if(result.hasErrors()){
+            model.addAttribute("student",studentDto);
+            return "create_student";
+        }
+
         studentService.createStudent(studentDto);
         return "redirect:/students";
 
-
     }
-
+    //
 }
